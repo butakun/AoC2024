5d9f80350cf4fe494c584fab97eb59f660cdae89
Hiromasa kato <hka@trackman.com>
2024-12-05 07:36:48 +0100

    cleaned

--- 05/05_2.py
+++ 05/05_2.py
@@ -44,28 +44,9 @@ def check(update, rules):
     return True
 
 
-def apply(ii, rule, update, indices):
-    page1, page2 = rule
-    print(f"** rule {page1}|{page2}")
-    page = update[indices[ii]]
-    if page2 != page:
-        return ii
-
-    # page1 must be before page2 which is page
-    for j in indices[ii+1:]:
-        pagej = update[j]
-        if pagej == page1:
-            # put j in front of indices[ii]
-            print(f"  removing {j} and inserting it at {ii}: before: {indices=}")
-            indices.remove(j)
-            indices.insert(ii, j)
-            return ii + 1
-    return ii
-
-
 def apply2(rule, update, indices):
     page1, page2 = rule
-    print(f"** rule {page1}|{page2}: {indices=}")
+    logger.debug(f"** rule {page1}|{page2}: {indices=}")
     for ii, i in enumerate(indices):
         page = update[i]
         if page2 != page:
@@ -73,11 +54,12 @@ def apply2(rule, update, indices):
         for j in indices[ii+1:]:
             pagej = update[j]
             if page1 == pagej:
-                print(f"  removing {j} and inserting it at {ii}: before: {indices=}")
+                logger.debug(f"  removing {j} and inserting it at {ii}: before: {indices=}")
                 indices.remove(j)
                 indices.insert(ii, j)
                 return True
     return False
+
 
 def reorder(update, rules):
     indices = [i for i in range(len(update))]
@@ -87,27 +69,13 @@ def reorder(update, rules):
         for rule in rules:
             applied = apply2(rule, update, indices)
             if applied:
-                print(f"rule {rule[0]}|{rule[1]} applied: {indices=}, {[update[k] for k in indices]}")
+                logger.debug(f"rule {rule[0]}|{rule[1]} applied: {indices=}, {[update[k] for k in indices]}")
                 break
         if not applied:
             break
 
     reordered = [update[k] for k in indices]
     return reordered
-
-    """
-    ii = 0
-    while ii < N:
-        for rule in rules:
-            ii_new = apply(ii, rule, update, indices)
-            if ii_new != ii:
-                print(f"rule {rule[0]}|{rule[1]} applied: {indices=}, {[update[k] for k in indices]}")
-            #ii = ii_new
-        ii += 1
-
-    reordered = [update[k] for k in indices]
-    return reordered
-    """
 
 
 def main(inputfile):
@@ -118,14 +86,15 @@ def main(inputfile):
     for i, update in enumerate(updates):
         if check(update, rules):
             continue
-        print(f"reordering {i}: {update}")
+        logger.debug(f"reordering {i}: {update}")
         reordered = reorder(update, rules)
 
         l = len(reordered)
         assert l % 2 == 1
         imid = int(l / 2)
-        print(reordered[imid])
-        count += reordered[imid]
+        mid = reordered[imid]
+        logger.info(f"reordered: {reordered} -> {mid}")
+        count += mid
     print(count)
 
 
