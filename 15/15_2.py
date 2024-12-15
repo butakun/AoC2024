71e8816927c70a8072205fd064ee1eeef8608304
Hiromasa kato <hka@trackman.com>
2024-12-15 12:43:58 +0100

    tidy tidy

--- 15/15_2.py
+++ 15/15_2.py
@@ -33,124 +33,104 @@ def dump(grid):
         print("".join(l))
 
 
-def pushv(i, j, movement, grid):
-    idim, jdim = grid.shape[:2]
-    assert grid[i, j] == "[" or grid[i, j] == "]"
+def can_move_to(i, j, movement, grid):
+    if grid[i, j] == ".":
+        return True, []
+    elif grid[i, j] == "#":
+        return False, []
+
     if grid[i, j] == "]":
         j -= 1
+    assert grid[i, j] == "[" and grid[i, j+1] == "]"
 
-    if movement == "^":
-        di = -1
-    elif movement == "v":
-        di = 1
-    else:
-        raise ValueError(movement)
-
-    Q = [(int(i), int(j))]
+    Q = [(i, j)]
     B = []
     while Q:
         i, j = Q.pop()
         B.append((i, j))
-        i2 = i + di
-        if grid[i2, j] == "." and grid[i2, j+1] == ".":
-            continue
-        if grid[i2, j] == "[":
-            Q.append((i2, j))
-        elif grid[i2, j] == "#" or grid[i2, j+1] == "#":
-            return None
-        else:
-            if grid[i2, j] == "]":
-                Q.append((i2, j-1))
-            if grid[i2, j+1] == "[":
-                Q.append((i2, j+1))
 
-    logger.debug(f"  these can be pushed: {B}")
-    for box in B:
-        i, j = box
-        grid[i, j:j+2] = "."
-    for box in B:
-        i, j = box
-        grid[i+di, j] = "["
-        grid[i+di, j+1] = "]"
-    return True
+        if movement == ">":
+            if grid[i, j+2] == "#":
+                return False, []
+            if grid[i, j+2] == "[":
+                Q.append((i, j+2))
+        elif movement == "<":
+            if grid[i, j-1] == "#":
+                return False, []
+            if grid[i, j-1] == "]":
+                Q.append((i, j-2))
+        elif movement == "v":
+            if grid[i+1, j] == "#" or grid[i+1, j+1] == "#":
+                return False, []
+            if grid[i+1, j] == "[":
+                Q.append((i+1, j))
+            elif grid[i+1, j] == "]":
+                Q.append((i+1, j-1))
+            if grid[i+1, j+1] == "[":
+                Q.append((i+1, j+1))
+        elif movement == "^":
+            if grid[i-1, j] == "#" or grid[i-1, j+1] == "#":
+                return False, []
+            if grid[i-1, j] == "[":
+                Q.append((i-1, j))
+            elif grid[i-1, j] == "]":
+                Q.append((i-1, j-1))
+            if grid[i-1, j+1] == "[":
+                Q.append((i-1, j+1))
 
-
-def push(i, j, movement, grid):
-    idim, jdim = grid.shape[:2]
-    if movement == "<":
-        try:
-            j1 = next(j1 for j1 in range(j, 0, -1) if grid[i, j1] != "[" and grid[i, j1] != "]")
-        except StopIteration:
-            return False
-        if grid[i, j1] == "#":
-            return False
-        assert grid[i, j1] == "."
-        grid[i, j1:j] = grid[i, j1+1:j+1]
-    elif movement == ">":
-        try:
-            j2 = next(j2 for j2 in range(j, jdim) if grid[i, j2] != "[" and grid[i, j2] != "]")
-        except StopIteration:
-            return False
-        if grid[i, j2] == "#":
-            return False
-        assert grid[i, j2] == "."
-        grid[i, j:j2+1] = grid[i, j-1:j2]
-    elif movement == "^":
-        return pushv(i, j, movement, grid)
-    elif movement == "v":
-        return pushv(i, j, movement, grid)
-
-    return True
+    return True, B
 
 
 def step(robot, grid, movement):
     idim, jdim = grid.shape[:2]
     i, j = robot
-    i0, j0 = i, j
+    i1, j1 = i, j
 
     if movement == "<":
-        j -= 1
+        di, dj = 0, -1
     elif movement == ">":
-        j += 1
+        di, dj = 0, 1
     elif movement == "^":
-        i -= 1
+        di, dj = -1, 0
     elif movement == "v":
-        i += 1
+        di, dj = 1, 0
     else:
         raise ValueError(movement)
 
-    if grid[i, j] == "#":
-        return i0, j0
+    i2, j2 = i1 + di, j1 + dj
+    logger.debug(f"moving attempt {movement}, {i2}, {j2}")
 
-    if grid[i, j] == ".":
-        grid[i0, j0] = "."
-        grid[i, j] = "@"
-        return i, j
+    move, boxes = can_move_to(i2, j2, movement, grid)
+    if not move:
+        return i1, j1
 
-    assert grid[i, j] == "[" or grid[i, j] == "]"
-
-    logger.debug(f"moving attempt {movement}, {i}, {j}")
-    if push(i, j, movement, grid):
-        grid[i0, j0] = "."
-        grid[i, j] = "@"
-        return i, j
-    return i0, j0
-
+    to = [(i_+di, j_+dj) for i_, j_ in boxes]
+    logger.debug(f"  {boxes} can move to {to}")
+    for i, j in boxes:
+        grid[i, j] = "."
+        grid[i, j+1] = "."
+    for i, j in to:
+        grid[i, j] = "["
+        grid[i, j+1] = "]"
+    grid[i1, j1] = "."
+    grid[i2, j2] = "@"
+    return i2, j2
 
 
 def main(inputfile):
     grid, movements = read(inputfile)
 
     i, j = np.where(grid == "@")
-    i, j = i[0], j[0]
+    i, j = int(i[0]), int(j[0])
     assert grid[i, j] == "@"
     robot = [i, j]
 
+    dump(grid)
     for movement in movements:
         logger.debug(f"move {movement}, {robot=}")
         robot = step(robot, grid, movement)
-    dump(grid)
 
+    dump(grid)
     total = 0
     boxes = np.stack(np.where(grid == "["), axis=-1)
     for i, j in boxes:
